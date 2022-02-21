da0e3d7e3f4df71f914ca0ccd8cffcc5b06d2c8e
DarkPhoenix2704 <anbarasun123@gmail.com>
2022-02-21 18:50:05 +0530

    Added Infix to Postfix

---
+++ infixtopostfix.c
@@ -0,0 +1,115 @@
+#include<stdio.h>
+#include<string.h>
+
+void push(char);
+void push1(int);
+int priority(char);
+void read();
+int top = -1, top1 = -1, j = 0, i, x, y;
+char stk[50], stk1[50], a[50], ch, post[50];
+void main() {
+    printf("\n\n\tProgram for Infix to Postfix Evaluation");
+    printf("\n\t----------------------------------------\n");
+    printf("\n\tEnter the expression: ");
+    gets(a);
+    for (i = 0; a[i] != '\0'; i++) {
+        ch = a[i];
+        switch (ch) {
+        case '(':
+            push(ch);
+            break;
+        case ')':
+            while (stk[top] != '(') {
+                post[j++] = stk[top];
+                top--;
+            }
+            top--;
+            break;
+        case '+':
+        case '-':
+        case '^':
+        case '/':
+        case '*':
+            if (top == -1 || stk[top] == '(')
+                push(ch);
+            else {
+                x = priority(ch);
+                y = priority(stk[top]);
+                if (y >= x) {
+                    post[j++] = stk[top];
+                    top--;
+                    push(ch);
+                } else
+                    push(ch);
+            }
+            break;
+        default:
+            if (isalpha(ch))
+                post[j++] = ch;
+            break;
+        }
+    }
+    while (stk[top] != '\0') {
+        post[j++] = stk[top];
+        top--;
+    }
+    post[j] = '\0';
+    printf("\n\tPostfix expression: ");
+    puts(post);
+    read();
+}
+void push(char ch) {
+    top++;
+    stk[top] = ch;
+}
+void push1(int ch) {
+    top1++;
+    stk1[top1] = ch;
+}
+int priority(char c) {
+    if (c == 't' || c == '-')
+        return 1;
+    else if (c == '*' || c == '/')
+        return 2;
+    else if (c == '^')
+        return 3;
+    else
+        return 0;
+}
+void read() {
+    int c, o1, o2;
+    for (i = 0; post[i] != '\0'; i++) {
+        ch = post[i];
+        if (isalpha(ch)) {
+            printf("\n\tEnter the value for %c: ", ch);
+            scanf("%d", & c);
+            push1(c);
+        } else {
+            o1 = stk1[top1];
+            top1--;
+            o2 = stk1[top1];
+            top1--;
+            switch (ch) {
+            case '+':
+                x = o1 + o2;
+                break;
+            case '-':
+                x = o1 - o2;
+                break;
+            case '*':
+                x = o1 * o2;
+                break;
+            case '/':
+                x = o1 / o2;
+                break;
+            case '^':
+                x = o1 ^ o2;
+                break;
+            default:
+                break;
+            }
+            push1(x);
+        }
+    }
+    printf("\n\tValue of the expression is %d", stk1[top1]);
+}
