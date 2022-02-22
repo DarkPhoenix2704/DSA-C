0bd4880366b1f0bb6b6f7a1d88c54a8a16bf8f81
Anbarasu <anbarasun123@gmail.com>
2022-02-22 14:15:03 +0530

    Update linearsearch.c

--- linearsearch.c
+++ linearsearch.c
@@ -1,26 +1,24 @@
 #include<stdio.h>
 
 void main() {
-    int a[50], i, n, elt, count;
-    printf("\n\tLINEAR SEARCH");
-    printf("\n\t________________\n\n");
-    printf("\n\tEnter the limit:");
-    scanf("%d", & n);
-    printf("\n\tEnter the elements:");
-    for (i = 0; i < n; i++) {
-        scanf("%d", & a[i]);
+    int arr[50], i, num, size, flag = -1;
+    printf("Enter the size of the array:- ");
+    scanf("%d", & size);
+    printf("Enter the elements of the array\n");
+    for (i = 0; i < size; i++) {
+        scanf("%d", & arr[i]);
     }
-    printf("\n\tThe elements are:");
-    for (i = 0; i < n; i++) {
-        printf("\t%d", a[i]);
-    }
-    printf("\n\n\tEnter the element to be searched:");
-    scanf("%d", & elt);
-    count = 0;
-    for (i = 0; i < n; i++) {
-        if (a[i] == elt) {
-            count = count + 1;
+    printf("\n\nEnter the element to be searched:- ");
+    scanf("%d", & num);
+    for (i = 0; i < size; i++) {
+        if (num == arr[i]) {
+            flag = 0;
+            break;
         }
     }
-    printf("\n\n\tThe element is present %d times", count);
+    if (flag == -1) {
+        printf("\nElement is not found!!!!");
+    } else {
+        printf("Element is found at %d", i + 1);
+    }
 }
